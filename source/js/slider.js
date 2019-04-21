4c2c1a8dcd3870034570eda6bc9fd224da028ced
Danil Chernov <danilchernov-developer@yandex.ru>
2019-04-21 21:56:59 +0700

    edge block with slider for mobile

---
+++ source/js/slider.js
@@ -0,0 +1,25 @@
+;(function($) {
+  'use strict';
+  /* Check viewport. Slider will be init only in below 768px of viewport */
+  const $sliders = $('[js-slider]');
+  if ($sliders.length != 0) {
+    let documentWidth = $(document).innerWidth();
+    if (documentWidth < 768) {
+      $sliders.each(function() {
+        const $slider = $(this);
+        $slider.addClass('owl-carousel');
+        $slider.owlCarousel({
+          dotsClass: 'slider__dots',
+          dotClass: 'slider__dot',
+          items: 1,
+          dots: true,
+          loop: true,
+          autoplay: true,
+          autoplayTimeout: 5000,
+          autoplaySpeed: 500
+        });
+      });
+    }
+  }
+
+}(jQuery));
