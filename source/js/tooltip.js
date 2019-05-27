5612d7004ae1bdae444c0d2a85ff09bcf8e4b3a5
Danil Chernov <danilchernov-developer@yandex.ru>
2019-05-27 17:09:36 +0700

    Fixed section of benefits in the desktop, fixed glucose in the graph in the catalog in .intro. Added a page about the company

--- source/js/tooltip.js
+++ source/js/tooltip.js
@@ -1,7 +1,15 @@
 ;(function($) {
   $('.tooltip').tooltipster({
     theme: 'tooltipster-punk',
-    trigger: 'click',
-    zIndex: 3
+    trigger:"custom",
+    triggerOpen: {
+      click: true,  // For mouse
+      tap: true    // For touch device
+    },
+    triggerClose: {
+      click: true,  // For mouse
+      tap: true    // For touch device
+    },
+    zIndex: 3,
   });
 }(jQuery));
