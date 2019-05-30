e2b0066e8619bd62983ca19d93e9708eed5fce2b
Danil Chernov <danilchernov-developer@yandex.ru>
2019-05-30 12:08:50 +0700

    Added cropped diplomas, updated call icon, fixed bug with prices in catalog

--- configurations/tasks/images.js
+++ configurations/tasks/images.js
@@ -6,7 +6,6 @@ const config = require('../config');
 
 const imageminPngquant = require('imagemin-pngquant');
 const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminGiflossy = require('imagemin-giflossy');
 
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
@@ -19,15 +18,12 @@ module.exports = () => {
           speed: 1,
           quality: 85
         }),
-        $.imagemin.gifsicle({
+        /* 
+            ! without Gifs !
+            $.imagemin.gifsicle({
           interlaced: true,
           optimizationLevel: 3
-        }),
-        imageminGiflossy({
-          optimizationLevel: 3,
-          optimize: 3,
-          lossy: 2
-        }),
+        }), */
         $.imagemin.jpegtran({
           progressive: true
         }),
